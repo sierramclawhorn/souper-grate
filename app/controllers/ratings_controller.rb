f5d96a0037bac91ab983e9e9b6aee65c07244e94
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-17 11:55:47 -0800

    Add index & create routes

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,18 @@
 class RatingsController < ApplicationController
+  def index
+    @ratings = Rating.all
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @recipe = @category.recipes.find(parame[:id])
+    @rating = Rating.new(params.require(:rating).permit(:rating))
+
+    if @rating.save
+      redirect_to category_recipe_path
+      flash[:notice] = 'Rating successfully saved.'
+    else
+      flash[:notice] = 'Unable to save rating.'
+    end
+  end
 end
