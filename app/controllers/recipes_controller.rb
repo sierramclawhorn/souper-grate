b011d5ab3d73ca88b49d94b83156f89a1eaadda4
Timothy Tao <timctao@gmail.com>
2016-11-17 11:14:04 -0800

    Define recipe show route, create recipe show view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,24 @@
 class RecipesController < ApplicationController
+  def index
+  end
+
+  def show
+    @category = Category.find(params[:category_id])
+    @recipe = @category.recipes.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
