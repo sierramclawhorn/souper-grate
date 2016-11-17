4df9e4fe89dc1c9084cddbd07cffa86f1f18cf7e
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-17 14:36:34 -0800

    Add new rating to show route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,14 @@ class RecipesController < ApplicationController
   def show
     @category = Category.find(params[:category_id])
     @recipe = @category.recipes.find(params[:id])
+    @rating = Rating.new
+
+    if @rating.save
+      redirect_to category_recipe_path
+      flash[:notice] = 'Rating successfully saved.'
+    else
+      flash[:notice] = 'Unable to save rating.'
+    end
   end
 
   def new
