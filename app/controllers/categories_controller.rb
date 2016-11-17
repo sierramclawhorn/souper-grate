90c38b3f3c27903bb4005b4a7f48d93cf473b6bc
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-16 20:39:15 -0800

    Add recipes var; work in progress

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,6 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:category_id])
-    @recipe = category.recipes.find(params[:id])
+    @recipe = category.recipes.find(params[:id]) 
   end
 end
