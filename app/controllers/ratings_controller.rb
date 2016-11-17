3b7063bedfc9572ce521e413a88a955cf702eac1
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-17 15:24:00 -0800

    Redirect to recipe#show; Add comments for future todos

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,28 @@
 class RatingsController < ApplicationController
+  def index
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @recipe = @category.recipes.find(params[:recipe_id])
+    @rating = @recipe.ratings.new(rating_params)
+    @rating.user_id = current_user.id
+
+    if @rating.save
+      redirect_to [@category, @recipe] 
+      # ajax to have rate partial disappear
+      # limit user to rating each recipe only once
+    else
+      redirect_to [@category, @recipe]
+    end
+  end
+
+
+private
+def rating_params
+  params.require(:rating).permit(:rating)
 end
+
+end
+
+
