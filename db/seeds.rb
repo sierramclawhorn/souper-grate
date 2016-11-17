3f7e09b1faa0e2204b03eb9aca9fa9077faa0422
Sierra McLawhorn <sierramclawhorn@gmail.com>
2016-11-16 16:53:30 -0800

    Add seeding for 4 categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Seeding for Categories:
+categories = Category.create([
+  { type: 'Appetizers'},
+  { type: 'Desserts'},
+  { type: 'Main Courses'},
+  { type: 'Soups & Salads'}
+  ])
